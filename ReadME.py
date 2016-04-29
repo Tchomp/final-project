299d6d2c394507507a94fd681dbc16f1f0ac68bd
Tchomp <tswain2@mail.valenciacollege.edu>
2016-04-28 22:46:29 -0400

    Readme!

---
+++ ReadME.py
@@ -0,0 +1,41 @@
+#Timothy Swain
+#Intro/Demo-Game/Final Project
+
+#Now, looking at the game, I'm sure some people are going to be confused as to what commands are usable for each section and part. Moreover, this file will
+#discuss some of the pieces that were used throughout the process of making this program.
+
+#Firstly, this program is not the most complex of files, it is a long, and tedious to read file, as it sequences multiple events that are not squared down,
+#usually, into 'else' when it doesn't need to be. Most of the program runs off of the simple variables and input program, using many, many, many variables
+#in order to make sure nothing interacts negatively with the other.
+
+#Before I start going into inputs, I want the reader to know the program will crash if an input is used that is not selectable. As such, the text tends to
+#point evidence torwards what commands should be typed.
+
+#Next: These are the following selectable inputs at certain scenes.
+
+#Door Choices: "Left", "Right", "Front"
+
+#Left Door Choices:
+
+#Answer the Goblin's Question: "Apple", and any other input.
+#Answer the Goblin's Offer: "Yes", "No", and any other input.
+#Eating the Apple: "Pretend", "Eat".
+#Left Door Giant's Quiz: "Giant", "Human", and any other input.
+#Left Door Giant's Fight: "Run Away", "Fight Giant", "Run for Sword".
+
+#Front Door Choices:
+
+#Mysterious Puzzle Room: "Touch Red Orb", "Touch Yellow Orb", "Touch Blue Orb", "Look Around", "Move Forward".
+#Front Door Giant's Quiz: "Giant", "Human", and any other input.
+#Front Door Giant's Fight: "Run Away", "Fight Giant", "Run for Sword".
+
+#Right Door Choices:
+
+#Large Wolf Encounter: "Sneak", "Pet".
+#Small Dogs Encounter: "Pet Dogs", "Run Past Dogs", "Yell at Dogs", "Engage Dog Fight".
+#Right Door Giant's Quiz: "Giant", "Human", and any other input.
+#Right Door Giant's Fight: "Run Away", "Fight Giant", "Run for Sword". 
+
+#That is all the current encounters for this dungeon! In my spare time, I hope to work on this adding more and more features that I have learned about in
+#the Python languages. One day, I will expand this dungeon to have far more rooms, and much less repetitive encounters, much more meaningful puns, and much
+#better dialogue options that improve along with my writing. Thank you for reading this, and hope you have a pleasant day! 
